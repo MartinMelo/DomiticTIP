410d3e0484d07f867957a4f1555bd91c1829a4d0
Martin Melo <alejandromelo2005@gmail.com>
2014-10-07 20:22:08 -0300

    rutas de control arregladas

--- public/modules/controls/config/controls.client.config.js
+++ public/modules/controls/config/controls.client.config.js
@@ -5,8 +5,8 @@ angular.module('controls').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
 		Menus.addMenuItem('topbar', 'Control Empleados', 'controls', 'dropdown', '/controls(/create)?');
-		Menus.addSubMenuItem('topbar', 'controls', 'Llegada', 'controls/llegada');
-		Menus.addSubMenuItem('topbar', 'controls', 'Partida', 'controls/partida');
+		Menus.addSubMenuItem('topbar', 'controls', 'Llegada', 'controls/llegadas');
+		Menus.addSubMenuItem('topbar', 'controls', 'Partida', 'controls/partidas');
 		Menus.addSubMenuItem('topbar', 'controls', 'Reportes', 'controls/reportes');
 	}
 ]);
