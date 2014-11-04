418fe05af8658413085b1bdbf3bad2207532c907
Martin Melo <alejandromelo2005@gmail.com>
2014-11-04 16:36:22 -0300

    Fix de la actulizacion de un dispositivo

--- public/modules/dispositivos/controllers/dispositivos.client.controller.js
+++ public/modules/dispositivos/controllers/dispositivos.client.controller.js
@@ -53,8 +53,10 @@ angular.module('dispositivos').controller('DispositivosController', ['$scope', '
 
 		// Update existing Dispositivo
 		$scope.update = function() {
-			var dispositivo = $scope.dispositivo ;
-            dispositivo.controlador = $scope.controlador;
+            var dispositivo = $scope.dispositivo;
+            if ($scope.controlador.indexOf('Seleccione')<0){
+                dispositivo.controlador = $scope.controlador;
+            }
 			dispositivo.$update(function() {
                 $scope.cambiarPagina($scope.urlList);
 			}, function(errorResponse) {
