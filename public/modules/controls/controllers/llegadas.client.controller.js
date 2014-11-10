e580c824261430aafeab122f8059174d14f93049
Martin Melo <alejandromelo2005@gmail.com>
2014-11-09 21:34:06 -0300

    Pequeña modificacion en como se muestran las llegadas y partidas. Me queda modificar el modelo del control para que se guarde la relacion con el empleado. Ya que no se hace esto.

--- public/modules/controls/controllers/llegadas.client.controller.js
+++ public/modules/controls/controllers/llegadas.client.controller.js
@@ -4,7 +4,7 @@ angular.module('controls').controller('LlegadasController', ['$scope','$http',
 	function($scope, $http) {
 
 
-
+        $scope.fecha = Date.now();
         $http.get('/controls/llegadas').success(function(data){
             $scope.llegadas = data;
         });
--- public/modules/controls/controllers/partidas.client.controller.js
+++ public/modules/controls/controllers/partidas.client.controller.js
@@ -3,6 +3,8 @@
 angular.module('controls').controller('PartidasController', ['$scope','$http',
 	function($scope, $http) {
 
+
+        $scope.fecha = Date.now();
         $http.get('/controls/partidas').success(function(data){
             $scope.partidas = data;
         });
