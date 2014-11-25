a3e8c065c2eb37307260bae4bbe147673cd22330
Martin Alejandro Melo <alejandromelo2005@gmail.com>
2014-11-24 23:38:46 -0300

    Elimine cosas del controller de empleados que no se usaba. Tambien cambie un dato de la lista de empleados porque no se mostraba.

--- public/modules/empleados/controllers/empleados.client.controller.js
+++ public/modules/empleados/controllers/empleados.client.controller.js
@@ -4,52 +4,6 @@
 angular.module('empleados').controller('EmpleadosController', ['$scope', '$stateParams', '$location', 'Authentication', 'Empleados',
 	function($scope, $stateParams, $location, Authentication, Empleados ) {
 		$scope.authentication = Authentication;
-
-		// Create new Empleado
-		$scope.create = function() {
-			// Create new Empleado object
-			var empleado = new Empleados ({
-                nombre: this.nombre
-			});
-
-			// Redirect after save
-			empleado.$save(function(response) {
-				$location.path('empleados/' + response._id);
-			}, function(errorResponse) {
-				$scope.error = errorResponse.data.message;
-			});
-
-			// Clear form fields
-			this.nombre = '';
-		};
-
-		// Remove existing Empleado
-		$scope.remove = function( empleado ) {
-			if ( empleado ) { empleado.$remove();
-
-				for (var i in $scope.empleados ) {
-					if ($scope.empleados [i] === empleado ) {
-						$scope.empleados.splice(i, 1);
-					}
-				}
-			} else {
-				$scope.empleado.$remove(function() {
-					$location.path('empleados');
-				});
-			}
-		};
-
-		// Update existing Empleado
-		$scope.update = function() {
-			var empleado = $scope.empleado ;
-
-			empleado.$update(function() {
-				$location.path('empleados/' + empleado._id);
-			}, function(errorResponse) {
-				$scope.error = errorResponse.data.message;
-			});
-		};
-
 		// Find a list of Empleados
 		$scope.find = function() {
 			$scope.empleados = Empleados.query();
